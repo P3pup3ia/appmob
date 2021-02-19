85f28affd8618eb6aefef8416abb23073b848c6e
Robert Sancts <robert.sacnts@gmail.com>
2021-02-19 10:07:06 -0300

    projeto incializado, criando a App

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from './style';
+
+const App: React.FC = () => (
+  <>
+    <Container />
+  </>
+);
+
+export default App;
---
+++ src/style.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  background: #8e8e8e;
+`;
