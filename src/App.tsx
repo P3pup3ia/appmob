7efaaf5dbbdff78d3c88e32a6758637576b6a522
Robert Sancts <robert.sacnts@gmail.com>
2021-02-19 11:49:05 -0300

    adicionando react-navigator e estilizando o background do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 
 import { View } from 'react-native';
+import { Container } from './style';
+import { NavigationContainer } from '@react-navigation/native';
 
-const App: React.FC = () => <View />;
+const App: React.FC = () => (
+  <>
+    <NavigationContainer>
+      <View>
+        <Container />
+      </View>
+    </NavigationContainer>
+  </>
+);
 
 export default App;
